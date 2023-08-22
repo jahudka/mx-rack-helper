6bf47f9738eb3b7335173c13c59233c941ec91ca
Dan Kadera <me@subsonic.cz>
2023-08-22 02:20:42 +0200

    init commit

--- src/cli.ts
+++ src/cli.ts
@@ -4,11 +4,24 @@ import { findMixer } from './findMixer';
 import { setPortAliases } from './reaper';
 
 (async () => {
-  const scanner = await findMixer();
-  await scanner.ensureSampleRate();
+  while (true) {
+    try {
+      const scanner = await findMixer();
+      await scanner.ensureSampleRate();
 
-  const outputs = await scanner.scanCardRouting();
-  await scanner.terminate();
+      const outputs = await scanner.scanCardRouting();
+      await scanner.terminate();
 
-  await setPortAliases(process.argv[2], outputs);
+      await setPortAliases(process.argv[2], outputs);
+
+      return;
+    } catch (e: any) {
+      if (e.code === 'ENETUNREACH') {
+        await new Promise((r) => setTimeout(r, 1000));
+        continue;
+      }
+
+      throw e;
+    }
+  }
 })();
